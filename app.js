37392ac7dd7d9da5fe7dad74438ee24bec1336d7
sasham43 <sashasemail@gmail.com>
2016-04-06 19:27:41 -0500

    Pro mode working, had to refactor code a lot

--- app.js
+++ app.js
@@ -4,19 +4,25 @@ console.log('app.js running');
 
 app.controller('MainController', function($scope){
   $scope.toDo = '';
-  $scope.toDoList = [];
+  // $scope.toDoList = [];
+  $scope.toDoObject = {};
+  $scope.count = 0;
 
   $scope.runClick = function(){
-    $scope.toDoList.push($scope.toDo);
-    console.log($scope.toDoList);
+    $scope.count++;
+    $scope.itemName = 'item' + $scope.count;
+    $scope.toDoObject[$scope.itemName] = {item: $scope.toDo, remove: false};
+    console.log($scope.toDoObject);
     $scope.toDo = '';
   }
 
-  $scope.removeItem = function(item){
-    console.log(item);
-    $scope.toDoList.pop();
+  $scope.removeItem = function(){
+    for (item in $scope.toDoObject){
+      $scope.item = $scope.toDoObject[item];
+      if($scope.item.remove == true){
+        delete $scope.toDoObject[item];
+      }
+    }
     $scope.toDo = '';
   }
-
-
 });
